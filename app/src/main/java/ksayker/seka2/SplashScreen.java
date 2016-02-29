6987ee25552b10ee7418fd5836196e8fd2371c3a
Ksayker <Ksayker@gmail.com>
2016-02-29 10:09:47 +0200

    worked version.

---
+++ app/src/main/java/ksayker/seka2/SplashScreen.java
@@ -0,0 +1,63 @@
+package ksayker.seka2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+/**
+ * Created by Yurec on 29.02.2016.
+ */
+public class SplashScreen extends Activity {
+    //how long until we go to the next activity
+    protected int _splashTime = 2000;
+
+    private Thread splashTread;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        final SplashScreen sPlashScreen = this;
+
+        // thread for displaying the SplashScreen
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized(this){
+
+                        //wait 5 sec
+                        wait(_splashTime);
+                    }
+
+                } catch(InterruptedException e) {}
+                finally {
+                    finish();
+
+                    //start a new activity
+                    Intent i = new Intent();
+                    i.setClass(sPlashScreen, MainActivity.class);
+                    startActivity(i);
+
+                    finish();
+                }
+            }
+        };
+
+        splashTread.start();
+    }
+
+    //Function that will handle the touch
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            synchronized(splashTread){
+                splashTread.notifyAll();
+            }
+        }
+        return true;
+    }
+}
